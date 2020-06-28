3c1edaa83feb1cd3ff9d266533bb8ae886ddf0bc
Patrick Dill <jamespatrickdill@gmail.com>
2020-06-27 23:24:32 -0400

    environment variable test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -39,10 +40,11 @@ class MyConfig(figa.Config):
     hocon_example = str(config_root / "config.conf")
 
     env_example = "env", "cfg_"  # environment variables with cfg_ prefix
-    web_example = "http", "https://support.oneskyapp.com/hc/en-us/article_attachments/202761627/example_1.json"
+    web_example = "http", "https://gist.githubusercontent.com/jpatrickdill/" \
+                          "7941fc1b531e69dfd4fe96c3628c497b/raw/8c49dba146f533614bee2b85ac0aa379de79ca99/config.json"
 
 
-tests = ["yaml", "ini", "json", "toml", "hocon"]
+tests = ["yaml", "ini", "json", "toml", "hocon", "web"]
 
 
 @pytest.mark.parametrize("environment", tests)
@@ -52,6 +54,44 @@ def test_configs(environment):
     assert (not DeepDiff(config.raw(), test_against[0])) or (not DeepDiff(config.raw(), test_against[1]))
 
 
-def test_no_env():
+# noinspection PyTypeChecker
+def test_env_vars():
+    # environment variables to test
+    env_vals = {
+        "key": "env_value",
+        "creds": {
+            "user": "usernamehere",
+            "pass": "supersecret"
+        }
+    }
+
+    # set environment variables using _ prefix for sub-dicts
+    def set_env(d, prefix=""):
+        for k, v in d.items():
+            if isinstance(v, dict):
+                set_env(v, prefix=prefix + k + "_")
+            else:
+                os.environ[prefix + k] = str(v)
+
+    # check using sub-dicts of original variables
+    def env_eq(cfg, d):
+        for k, v in d.items():
+            if isinstance(v, dict):
+                if not env_eq(cfg[k], d[k]):
+                    return False
+
+            elif str(v) != cfg[k]:
+                return False
+
+        return True
+
+    set_env(env_vals, "cfg_")
+
+    config = MyConfig("env_example")
+
+    assert env_eq(config, env_vals)
+
+
+def test_nonexistant_env():
     with pytest.raises(ValueError):
         config = MyConfig("nonexistant_env")
